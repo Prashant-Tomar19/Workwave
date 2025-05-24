57cdfb68ce3492711e0900166fa6f4028a2b253e
Prashant-Tomar19 <prashanttomar7581@gmail.com>
2025-05-24 16:56:14 +0530

    added js to page3,4

---
+++ script.js
@@ -0,0 +1,16 @@
+function openModal(id) {
+  document.getElementById(id).style.display = 'block';
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = 'none';
+}
+
+// Close modal when clicking outside the modal content
+window.onclick = function(event) {
+  document.querySelectorAll('.modal').forEach(modal => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+}
